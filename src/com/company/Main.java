1731c7a1a41917439744b3f060cc19c19c3291e5
AnnaDremluga <dreanna3@gmail.com>
2021-06-16 22:28:30 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String s1 = "This is exercise 1";
+        String s1 = "This is exercise 1"; // length of all names must be longer than 1 and should be longer than 2. For example excerciseLowerCase or just lowerCaseEx
         String s2 = "This is Exercise 1";
         String s3 = "This is not Exercise 1";
-        boolean flag;
+        boolean flag;       //  better to name variables by meaning. For example, boolean areCaseInsensitiveEqual or areTotallyEqual or just areEqual
 
         flag = s1.equalsIgnoreCase(s2);
-        System.out.println("Is s1 equal to s2 ? Its " + flag);
+        System.out.println("Is s1 equal to s2 ? Its " + flag); // you can optimize and avoid creating of excess variable
 
         flag = s1.equalsIgnoreCase(s3);
-        System.out.println("Is s1 equal to s2 ? Its " + flag);
+        System.out.println("Is s1 equal to s2 ? Its " + flag); // you have a bug here
 
     }
 }
